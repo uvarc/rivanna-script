447ecede29a8eb7020ebc1938953e4fb069f0c7c
khs3z <khs3z@virginia.edu>
2024-02-02 01:04:23 -0500

    advanced filtering

--- usage-stats/core-usage-summary.py
+++ usage-stats/core-usage-summary.py
@@ -109,10 +109,10 @@ def merge_data(labels, usage_file, account_file, org_file, capacity_file, hours,
 
 	org_groups = [g for g in groups if g in usage_df.columns.values]
 	usage_df = usage_df.groupby(org_groups).sum().reset_index()
-	print(usage_df.head())
+	print(usage_df)
 
-	account_df = pd.read_csv(account_file, delimiter=r"\s+", header=0)
 	org_df = pd.read_csv(org_file, delimiter=r"\s+", header=0, names=['Organization', 'School'])
+	account_df = pd.read_csv(account_file, delimiter=r"\s+", header=0)
 	cols = account_df.columns.values
 	cols[0] = "Allocation"
 	account_df.columns = cols
@@ -126,51 +126,109 @@ def merge_data(labels, usage_file, account_file, org_file, capacity_file, hours,
 	return combined
 
 
-def filter_by_school(df: pd.DataFrame, school: str, filepath=".", fname="rivanna-stats") -> None:
-	path = os.path.join(filepath, school)
-	os.makedirs(f"{path}", exist_ok=True)
-	# year and date in output file : rivanna-stats-YEAR-DATE
-	if school == "all" or "School" not in df.columns:
-		df.to_csv(f"{path}/{fname}", index=False)
+def save_df(df: pd.DataFrame, filepath=".", fname="rivanna-stats") -> None:
+	path = os.path.join(filepath, fname)
+	os.makedirs(filepath, exist_ok=True)
+	df.to_csv(path, index=False)
+
+
+def apply_filter(df: pd.DataFrame, filter_dict) -> pd.DataFrame:
+	print (f"Applying filter {filter_dict} to columns={df.columns.values}")
+	if "all" in filter_dict.keys():
+		# nothing to do 
+		return df
+	# drop keys that don't exist in dataframe
+	filter_dict = {k:v for k,v in filter_dict.items() if k in df.columns.values}
+
+	if len(filter_dict) == 0:
+		# filter column not present, nothing to do
+		return pd.DataFrame(columns=df.columns.values)
+
+	print (f"filter_dict={filter_dict}")
+	if len(filter_dict) == 1:
+		# filter by single column
+		key_col = list(filter_dict.keys())[0]
+		values = filter_dict[key_col]
+		filtered_df = df[df[key_col].isin(values)] 
 	else:
-		df[df["School"] == school].to_csv(f"{path}/{fname}", index=False)
+		# create multi-index from filter dict and dataframe -> keep overlap
+		fvalue_list = [v for v in filter_dict.values()]
+		filter_idx = pd.MultiIndex.from_product(fvalue_list, names=list(filter_dict)) 
+		df_idx = pd.MultiIndex.from_frame(df[filter_dict.keys()])
+		filtered_df = df.loc[df_idx.isin(filter_idx)]
+	print (filtered_df.groupby(list(filter_dict)).sum(numeric_only=True))
+	return filtered_df
+
+
+def parse_filter(farg: str) -> list:
+	"""Example:
+	    in:
+		"School:[DS,EN,MD];Description:[standard,purchase]|Status:[Staff,Faculty]"
+
+		out: list of dict
+		[
+			{
+				'School': ['DS', 'EN', 'MD'], 
+				'Description': ['standard', 'purchase']
+			}, 
+			{
+				'Status': ['Staff', 'Faculty']
+			}
+		]
+	"""
+	filters = farg.split("|")
+	filter_list = []
+	for f in filters:
+		items = f.split(";")
+		fd = {}
+		for item in items:
+			kv = item.split(":")
+			if len(kv) > 1:
+				fd[kv[0]] = kv[1].replace("[", "").replace("]", "").split(",")
+			else:
+				fd[kv[0]] = None
+		filter_list.append(fd) 
+	if not any("all" in d for d in filter_list):
+		filter_list.append({"all":None})
+	return filter_list    
 
 
 if __name__ == '__main__':
 	parser = init_parser()
 	args = parser.parse_args()
-	school_filters = args.filter.split(",") # initialize filter variable
-	if 'all' not in school_filters: school_filters.append('all')
+	filters = parse_filter(args.filter)
 	agroups = list(set(args.groups.replace('|', ',').split(',')))
 	if 'Allocation' not in agroups:
 		agroups.append('Allocation')
-	print(f"agroups={agroups}")
 	analysis = args.groups.split('|')
 	hours = float(args.days) * 24
 	df = merge_data(args.labels, args.usage, args.allocations, args.organizations, args.capacity, hours, groups=agroups)
-	#df.to_csv(args.output, index=False)
-
-	for school in school_filters:
-		filter_by_school(df, school, filepath=args.path, fname=args.output)
+	filters = parse_filter(args.filter)
 
 	for r in analysis:
 		print(''.join(["#"] * 80))
 		groups = r.split(',') if args.groups != '' else ['Allocation']
 		print(f'Analyzing by {r}, {groups}')
-		for school in school_filters:
+		for filter in filters:
+			print (f"Filtering by {filter}")
 			sum_df = df.groupby(groups).sum().reset_index()  # does this need to be inside inner loop
 			ftrunk, ext = os.path.splitext(args.output)
-			fname = f"{ftrunk}-{''.join(groups)}-{school}{ext}"
-			print(fname)
-			#if "School" not in sum_df.columns:
-			#	continue
-			filter_by_school(sum_df, school, filepath=args.path, fname=fname)
+			# flatten filter values which is a list of lists
+			if list(filter.values())[0] is not None:
+				f_values = [v for values in filter.values() for v in values]
+				filter_str = "-".join(f_values)
+			else:
+				filter_str = "-".join(filter.keys())
+			print (f"filter_str={filter_str}")
+			filepath = args.path.replace("{FILTER}", filter_str)
+			fname = f"{ftrunk}-{''.join(groups)}-{filter_str}{ext}"
+			print(f"filepath={filepath}, fname={fname}")
+			filtered_df  = apply_filter(sum_df, filter)
+			save_df(filtered_df, filepath=filepath, fname=fname)
 			
-			# These print statements are meaningless if repeated on the same unfiltered sum_df
-			# print(sum_df)
-			# print(sum_df.sum())
-			# print("------------------------------------")
-			# print(f"Total CPU Hours: {df['Total CPU hours'].sum():,.2f}")
-			# print(f"Total GPU Device Hours: {df['Total GPU hours'].sum():,.2f}")
+			print(filtered_df)
+			print("------------------------------------")
+			print(f"Total CPU Hours: {filtered_df['Total CPU hours'].sum():,.2f}")
+			print(f"Total GPU Device Hours: {filtered_df['Total GPU hours'].sum():,.2f}")
 			
 			groups = r.split(',') if args.groups != '' else ['User']
