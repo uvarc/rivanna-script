d65a6cf4f0375890bc68d3a0a8710d0a4fe9fb3b
Matthew Edward Galitz <ykk6rh@virginia.edu>
2024-03-15 10:12:19 -0400

    remove ml statements

--- usage-stats/monthly-report.sh
+++ usage-stats/monthly-report.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# update condition to account for FILTER arg
 if [ "$#" -ne 5 ]; then
     echo "Usage: `basename $0` YYYY MM DAYS FILTER OUTPUTPATH"
     exit 1
@@ -9,7 +8,7 @@ fi
 SYEAR=$1
 SMONTH=$2
 DAYS=$3
-FILTER=$4 # new assignment
+FILTER=$4 
 OUTPUTPATH=$5
 
 SDAY=01
@@ -20,22 +19,12 @@ echo "Reporting $FIRSTDAYMONTH through $LASTDAYMONTH"
 echo "Writing to $OUTPUTPATH"
 echo "Filtering: $FILTER"
 
-module purge
-module load anaconda
 newusers-report.sh ${SYEAR} ${SMONTH} ${OUTPUTPATH}/newusers-${SYEAR}-${SMONTH}.csv ${OUTPUTPATH}
 
 get-allocation-data.sh ${SYEAR} ${SMONTH} ${OUTPUTPATH}
-#su-transactions.py -f allocations.html -s ${SYEAR}-${SMONTH}-${SDAY} -e ${FIRSTNEXTMONTH} -o newallocations-${SYEAR}-${SMONTH}.csv
-#su-transactions.py -f allocations.html -s ${SYEAR}-${SMONTH}-${SDAY} -e ${FIRSTNEXTMONTH} -o newallocations-${SYEAR}-${SMONTH}-schooltype.csv -g "School,Type"
-#su-transactions.py -f allocations.html -s ${SYEAR}-${SMONTH}-${SDAY} -e ${FIRSTNEXTMONTH} -o newallocations-${SYEAR}-${SMONTH}-typeschool.csv -g "Type,School"
 
-module load anaconda
 core-usage-report.sh ${FIRSTDAYMONTH}T00:00:00 ${LASTDAYMONTH}T23:59:59 corehours-${SYEAR}-${SMONTH}.csv $DAYS "${FILTER}" $OUTPUTPATH
-# added $FILTER argument 
 
-# filter cpurawtime>0 (5th column in corehours-${SYEAR}-${SMONTH}.csv, and get uids
-#awk -F, '{ if ($2 > 0) print $1 }' all/corehours-${SYEAR}-${SMONTH}-userSchool-all.csv | head
 awk -F, '{ if ($2 > 0) print $1 }' ${OUTPUTPATH}/all/${SYEAR}-${SMONTH}/corehours-${SYEAR}-${SMONTH}-userSchool-all.csv | sort -u > ${OUTPUTPATH}/activeusers-UIDs-${SYEAR}-${SMONTH}.txt
 ldapreport.sh ${OUTPUTPATH}/activeusers-UIDs-${SYEAR}-${SMONTH}.txt > ${OUTPUTPATH}/activeusers-${SYEAR}-${SMONTH}.csv
-module load anaconda
 mergeusers.py ${OUTPUTPATH}/activeusers-${SYEAR}-${SMONTH}.csv ${OUTPUTPATH}
