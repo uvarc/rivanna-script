515395bcbd6291be05768a649cd909ff803e08fe
Matthew Edward Galitz <ykk6rh@virginia.edu>
2024-03-26 12:23:41 -0400

    remove var quotes

--- usage-stats/install.sh
+++ usage-stats/install.sh
@@ -7,15 +7,15 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-DIR="$1"
-PROJECT_PATH="$DIR/rivanna-script/usage-stats"
-PREFIX="$PROJECT_PATH/rivanna-util-env"
-YML="$PROJECT_PATH/environment.yml"
+DIR=$1
+PROJECT_PATH=$DIR/rivanna-script/usage-stats
+PREFIX=$PROJECT_PATH/rivanna-util-env
+YML=$PROJECT_PATH/environment.yml
 
 module load anaconda || { echo "Failed to load anaconda module"; exit 1 }
-conda env create -f "$YML" --prefix "$PREFIX" || { echo "Failed to create conda environment"; exit 1; }
+conda env create -f $YML --prefix $PREFIX || { echo "Failed to create conda environment"; exit 1; }
 module unload anaconda
 
-find "$PROJECT_PATH" -type f \( -name "*.sh" -o -name "*.py" \) -exec chmod 750 {} \;
+find $PROJECT_PATH -type f \( -name "*.sh" -o -name "*.py" \) -exec chmod 750 {} \;
 
 echo "Setup complete. Please activate the conda environment with 'ml anaconda && source activate $PREFIX' before use." 
