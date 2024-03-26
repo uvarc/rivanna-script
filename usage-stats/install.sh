65d53bd31ec293fae9d155d3a13165f50ae32185
Matthew Edward Galitz <ykk6rh@virginia.edu>
2024-03-26 12:00:23 -0400

    specify conda env prefix

--- usage-stats/install.sh
+++ usage-stats/install.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-echo "NOTE: run script at root level of cloned repository"
-
 if [ "$#" -ne 1 ]; then
-    echo "Usage: `basename $0` path/to/new-conda-env"
+    echo "Usage: `basename $0` path/to/repo"
     exit 1
 fi
 
 DIR=$1
-ENV_NAME="rivanna-util-env"
+PROJECT_PATH=$DIR/rivanna-script/usage-stats
+PREFIX=$PROJECT_PATH/rivanna-util-env
+YML=$PROJECT_PATH/environment.yml
 
-ml anaconda
-conda env create -f usage-stats/environment.yml -n $ENV_NAME # add conda env prefix w/ dir var
+module load anaconda
+conda env create -f $YML --prefix $PREFIX 
 module unload anaconda
-find $DIR -type f \( -name "*.sh" -o -name "*.py" \) -exec chmod 750 {} \;
+find $PROJECT_PATH -type f \( -name "*.sh" -o -name "*.py" \) -exec chmod 750 {} \;
 
-#LINE="export PATH=$DIR/rivanna-script/usage-stats/:$PATH"
-#FILE="$HOME/.bashrc"
-#grep -qxF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
-
-echo "Setup complete. Please activate the conda environment with 'ml anaconda && source activate $DIR/$ENV_NAME' before use." # double check how conda prefixes work
+echo "Setup complete. Please activate the conda environment with 'ml anaconda && source activate $PROJECT_PATH/$ENV_NAME' before use." 
