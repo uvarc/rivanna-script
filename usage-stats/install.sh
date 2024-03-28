250dfdc8a2f20c748d8cec1d4d9cf1bdbc118b92
Matthew Edward Galitz <ykk6rh@virginia.edu>
2024-03-28 19:56:08 -0400

    implement fail fast

--- usage-stats/install.sh
+++ usage-stats/install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-set -eo pipefail
-
 if [ "$#" -ne 1 ]; then
     echo "Usage: `basename $0` path/to/repo"
     exit 1
 fi
+
+set -e
 
 DIR=$1
 PROJECT_PATH=$DIR/rivanna-script/usage-stats
 PREFIX=$PROJECT_PATH/rivanna-util-env
 YML=$PROJECT_PATH/environment.yml
 
-module load anaconda || { echo "Failed to load anaconda module"; exit 1 }
-conda env create -f $YML --prefix $PREFIX || { echo "Failed to create conda environment"; exit 1; }
+module load anaconda 
+conda env create -f $YML --prefix $PREFIX 
 module unload anaconda
 
 find $PROJECT_PATH -type f \( -name "*.sh" -o -name "*.py" \) -exec chmod 750 {} \;
