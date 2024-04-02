8411ff70e6b496c1c0a5666ead13a046f51c7250
Matthew Edward Galitz <ykk6rh@virginia.edu>
2024-04-02 19:33:11 -0400

    activate conda env

--- usage-stats/schedule-monthly-report.sh
+++ usage-stats/schedule-monthly-report.sh
@@ -11,8 +11,13 @@ month=$(date --date="$(date +%Y-%m-15) -1 month" +%m)
 last_day=$(date -d "$year-$month-01 +1 month -1 day" +%d)
 
 filter="" # TODO: Change to desired filter
-output_dir="/absolute/path/to/output/dir" # TODO: Change to output directory where reports will be stored
+output_dir="/path/to/output/dir" # TODO: Change to output directory where reports will be stored
+conda_env="/path/to/conda/env" # TODO: Change to conda env created by install.sh
 
 SCRIPT="/absolute/path/to/monthly-report.sh" # TODO: Change this to the actual full path
 
+ml anaconda
+source activate $conda_env
 $SCRIPT $year $month $last_day $filter $output_dir
+conda deactivate
+module unload anaconda
