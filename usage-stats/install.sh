264cdaddc898908c1bc7d3c6089661f19c22cbe4
Matthew Edward Galitz <ykk6rh@virginia.edu>
2024-10-27 13:56:49 -0400

    Migrate from Anaconda to Miniforge

--- usage-stats/install.sh
+++ usage-stats/install.sh
@@ -12,10 +12,11 @@ PROJECT_PATH=$DIR/rivanna-script/usage-stats
 PREFIX=$PROJECT_PATH/rivanna-util-env
 YML=$PROJECT_PATH/environment.yml
 
-module load anaconda 
+module load miniforge 
 conda env create -f $YML --prefix $PREFIX 
-module unload anaconda
+module unload miniforge
 
 find $PROJECT_PATH -type f \( -name "*.sh" -o -name "*.py" \) -exec chmod 750 {} \;
 
-echo "Setup complete. Please activate the conda environment with 'ml anaconda && source activate $PREFIX' before use." 
+echo "Setup complete. Please activate the conda environment with 'ml miniforge && conda activate $PREFIX' before use." 
+echo "NOTE: execution of 'last-month-report.sh' does not require activating the conda environment"
